ee32172c972c8c9df967b98fba1a3d06151ff582
emerson correia <juniorcorreiajuniorcorreia@outlook.com.br>
2022-09-22 23:00:00 -0300

    adicionando projeto

--- src/Pages/projetos/projetos.js
+++ src/Pages/projetos/projetos.js
@@ -3,6 +3,7 @@ import Footer from "../../components/footer/footer";
 import Psite from "../../img/Site.png";
 import Pclima from "../../img/Clima.png";
 import Pcriacao from "../../img/criacao.png";
+import Pbruxo from "../../img/sitebruxo.png";
 import "./projetos.css";
 
 function Projetos() {
@@ -14,25 +15,27 @@ function Projetos() {
         <div className="containerProjetos">
           <div className="projeto">
             <a href="https://rqueiroz-tattoo.netlify.app/index.html">
-              <img src={Psite} alt="" />
+              <img src={Psite} alt="projeto" />
             </a>
           </div>
           <div className="projeto">
             <a href="https://github.com/ecjsilva/Aplicacao-de-Clima">
-              <img src={Pclima} alt="" />
+              <img src={Pclima} alt="projeto" />
+            </a>
+          </div>
+          <div className="projeto">
+            <a href="https://bruxo081.netlify.app/">
+              <img src={Pbruxo} alt="projeto" />
             </a>
           </div>
           <div className="projeto-criacao">
-            <img src={Pcriacao} alt="" />
+            <img src={Pcriacao} alt="projeto-criacao" />
           </div>
           <div className="projeto-criacao">
-            <img src={Pcriacao} alt="" />
+            <img src={Pcriacao} alt="projeto-criacao" />
           </div>
           <div className="projeto-criacao">
-            <img src={Pcriacao} alt="" />
-          </div>
-          <div className="projeto-criacao">
-            <img src={Pcriacao} alt="" />
+            <img src={Pcriacao} alt="projeto-criacao" />
           </div>
         </div>
       </div>
